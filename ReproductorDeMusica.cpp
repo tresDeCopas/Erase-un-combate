106fb4e8bffcc5e8dc47cba5f397e091f4f75aec
MascaChapas27 <127435083+MascaChapas27@users.noreply.github.com>
2025-07-20 14:55:50 +0200

    Cancioncillas para arreglar el ambiente

--- ReproductorDeMusica.cpp
+++ ReproductorDeMusica.cpp
@@ -1,5 +1,6 @@
 #include "ReproductorDeMusica.hpp"
 #include "Bitacora.hpp"
+#include <filesystem>
 
 ReproductorDeMusica * ReproductorDeMusica::reproductorDeMusica = nullptr;
 
@@ -10,21 +11,68 @@ ReproductorDeMusica * ReproductorDeMusica::unicaInstancia()
     return reproductorDeMusica;
 }
 
-void ReproductorDeMusica::reproducir(std::string cancion)
+ReproductorDeMusica::ReproductorDeMusica(){
+    volumenActual = VOLUMEN_MAXIMO_MUSICA;
+
+    if(!std::filesystem::is_directory("musica/combate")){
+        Bitacora::unicaInstancia()->escribir("Juan Cuesta: Oye Emilio... he notado que el directorio musica/combate es mÃ¡s liviano de lo habitial, Â¿no crees?");
+        Bitacora::unicaInstancia()->escribir("Emilio: Y tanto liviano, directamente no existe");
+        Bitacora::unicaInstancia()->escribir("Juan Cuesta: Esto es el acabose. Pongo una carpeta comunitaria para que la gente deje su mÃºsica y asÃ­ me lo pagan. No se puede tener talante...");
+        Bitacora::unicaInstancia()->escribir("Emilio: Pues nada, a pelear sin mÃºsica. Anda que... Vaya mierda de vecinos...");
+    } else {
+        for(const std::filesystem::directory_entry & entrada : std::filesystem::directory_iterator("musica/combate")){
+
+            // Es necesario comprobar que los cuatro Ãºltimos caracteres del string son ".wav"
+            std::size_t posicionPunto = entrada.path().string().size()-4;
+
+            if(entrada.path().string().substr(posicionPunto,4) == ".wav"){
+                cancionesCombate.push_back(entrada.path().string());
+            }
+        }
+    }
+
+    cancionMenu = "musica/menu-principal.wav";
+    cancionAjustes = "musica/ajustes.wav";
+    cancionFinRonda = "musica/fin-ronda.wav";
+}
+
+void ReproductorDeMusica::reproducir(std::string cancion, bool bucle)
 {
     if(!musicaActual.openFromFile(cancion)){
-        Bitacora::unicaInstancia()->escribir("Juan Cuesta: Emilio, vamos a animar un poco el ambiente, reproduce la canción " + cancion);
-        Bitacora::unicaInstancia()->escribir("Emilio: Ah no, esa se la llevó mi padre y ya no la he vuelto a ver");
-        Bitacora::unicaInstancia()->escribir("Juan Cuesta: Pero... ¿para qué le das a tu padre un bien común de esta, nuestra comunidad?");
+        Bitacora::unicaInstancia()->escribir("Juan Cuesta: Emilio, vamos a animar un poco el ambiente, reproduce la canciÃ³n " + cancion);
+        Bitacora::unicaInstancia()->escribir("Emilio: Ah no, esa se la llevï¿½ mi padre y ya no la he vuelto a ver");
+        Bitacora::unicaInstancia()->escribir("Juan Cuesta: Pero... ï¿½para quï¿½ le das a tu padre un bien comï¿½n de esta, nuestra comunidad?");
         Bitacora::unicaInstancia()->escribir("Emilio: Es que se puso muy pesado, y con tal de que me dejara un rato en paz...");
         Bitacora::unicaInstancia()->escribir("Juan Cuesta: Voy a encontrar a Mariano inmediatamente. Se suspende la junta.");
-        Bitacora::unicaInstancia()->escribir("Emilio: Pero no se enfade con él señor Juan, no es maldad, es que está demente ya.");
+        Bitacora::unicaInstancia()->escribir("Emilio: Pero no se enfade con ï¿½l, seï¿½or Juan. No es maldad, es que estï¿½ demente ya.");
         exit(EXIT_FAILURE);
     }
 
-    musicaActual.setLoop(true);
+    musicaActual.setLoop(bucle);
     musicaActual.setVolume(volumenActual);
     musicaActual.play();
+}
+
+void ReproductorDeMusica::reproducirCancionCombate(){
+
+    // Si no hay canciones de combate, no se reproduce nada
+    if(cancionesCombate.empty()) return;
+
+    int indiceCancion = rand() % (cancionesCombate.size());
+
+    reproducir(cancionesCombate[indiceCancion]);
+}
+
+void ReproductorDeMusica::reproducirCancionMenu(){
+    reproducir(cancionMenu);
+}
+
+void ReproductorDeMusica::reproducirCancionFinRonda(){
+    reproducir(cancionFinRonda,false);
+}
+
+void ReproductorDeMusica::reproducirCancionAjustes(){
+    reproducir(cancionAjustes);
 }
 
 void ReproductorDeMusica::detener()
@@ -32,12 +80,17 @@ void ReproductorDeMusica::detener()
     musicaActual.stop();
 }
 
-float ReproductorDeMusica::obtenerVolumen()
+float ReproductorDeMusica::getVolumen()
 {
     return musicaActual.getVolume();
 }
 
-void ReproductorDeMusica::establecerVolumen(float nuevoVolumen)
+bool ReproductorDeMusica::estaReproduciendo()
+{
+    return musicaActual.getStatus() == sf::Music::Status::Playing;
+}
+
+void ReproductorDeMusica::setVolumen(float nuevoVolumen)
 {
     musicaActual.setVolume(nuevoVolumen > VOLUMEN_MAXIMO_MUSICA ? VOLUMEN_MAXIMO_MUSICA : nuevoVolumen < 0 ? 0 : nuevoVolumen);
 }
--- ReproductorDeMusica.hpp
+++ ReproductorDeMusica.hpp
@@ -6,6 +6,7 @@
 #include "Enums.hpp"
 #include <string>
 #include <map>
+#include <vector>
 
 /*
     Clase que se encarga de reproducir música para que el
@@ -14,29 +15,60 @@
 class ReproductorDeMusica
 {
     private:
-        ReproductorDeMusica(){
-            volumenActual = VOLUMEN_MAXIMO_MUSICA;
-        }
 
+        // Constructor privado para que sea más Singleton
+        ReproductorDeMusica();
+
+        // Única instancia
         static ReproductorDeMusica * reproductorDeMusica;
 
+        // Música que se está reproduciendo actualmente
         sf::Music musicaActual;
-        std::map<std::string, std::string> rutasDeFicheros;
+
+        // Conjunto con todas las canciones de combate que hay
+        std::vector<std::string> cancionesCombate;
+
+        // Canción para el menú principal y el menú de selección de personajes
+        std::string cancionMenu;
+
+        // Canción para el menú de ajustes y el tutorial
+        std::string cancionAjustes;
+
+        // Canción que suena entre rondas
+        std::string cancionFinRonda;
+
+        // Volumen actual de la música (del 0 al 100)
         float volumenActual;
 
     public:
 
-        // Reproduce la canción dado su identificador
-        void reproducir(std::string cancion);
+        // Reproduce la canción dado su identificador (ruta del fichero relativa al directorio donde
+        // se ejecuta el programa)
+        void reproducir(std::string cancion, bool bucle = true);
+
+        // Reproduce aleatoriamente una canción de combate
+        void reproducirCancionCombate();
+
+        // Reproduce la canción del menú principal
+        void reproducirCancionMenu();
+
+        // Reproduce la canción del menú de ajustes
+        void reproducirCancionAjustes();
+
+        // Reproduce la canción que suena entre rondas
+        void reproducirCancionFinRonda();
 
         // Detiene por completo la reproducción de la canción
         void detener();
 
         // Devuelve el volumen de la canción actual
-        float obtenerVolumen();
+        float getVolumen();
 
         // Establece el nuevo volumen para la canción actual y las siguientes
-        void establecerVolumen(float nuevoVolumen);
+        void setVolumen(float nuevoVolumen);
+
+        // Dice si se está reproduciendo una canción o no
+        bool estaReproduciendo();
 
         // NUNCA SE COPIA UN SINGLETON
         ReproductorDeMusica(ReproductorDeMusica &otro) = delete;
