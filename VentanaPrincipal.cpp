8ca33f34cdd2bad1748bf0de578056e7f593db24
MascaChapas27 <127435083+MascaChapas27@users.noreply.github.com>
2025-07-20 14:21:37 +0200

    Framerate más estable a cambio de usar la CPU más

--- VentanaPrincipal.cpp
+++ VentanaPrincipal.cpp
@@ -10,11 +10,11 @@ sf::RenderWindow * VentanaPrincipal::unicaInstancia()
 {
     if(ventanaPrincipal==nullptr){
         ventanaPrincipal = new sf::RenderWindow(sf::VideoMode(VENTANA_ANCHURA, VENTANA_ALTURA), VENTANA_NOMBRE, VENTANA_ESTILO);
-        ventanaPrincipal->setFramerateLimit(VENTANA_FPS);
+        // ventanaPrincipal->setFramerateLimit(VENTANA_FPS);
         // Se desactiva lo de que si mantienes pulsada una tecla se vuelve a pulsar muchas veces
         // porque que pereza
         ventanaPrincipal->setKeyRepeatEnabled(false);
-        ventanaPrincipal->setVerticalSyncEnabled(true);
+        // ventanaPrincipal->setVerticalSyncEnabled(true);
 
         aumentarZoom();
     }
