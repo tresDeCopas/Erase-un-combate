126e7afc7abbf97bb6a1a2f49566b29d3b9e48b3
MascaChapas27 <127435083+MascaChapas27@users.noreply.github.com>
2025-07-20 15:02:18 +0200

    Comenzada implementación de ficheros YAML a partir de deformar el main.cpp hasta límites insospechados para hacer debug

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,53 @@
 #include "MenuPrincipal.hpp"
 #include "Combate.hpp"
 
+#include "yaml-cpp/yaml.h"
+#include <iostream>
+
 int main(int argc, char* argv[]){
+
+    YAML::Node config = YAML::LoadFile("config.yaml");
+
+    YAML::Node infoPersonaje = config["infoPersonaje"];
+
+    YAML::Node estados = config["estados"];
+
+    std::cout << "Personaje " << infoPersonaje["nombre"].as<std::string>() << ", con " << infoPersonaje["vida"].as<int>() << " puntos de vida, velocidad de " << infoPersonaje["velocidad"].as<int>() << " y fuerza de salto de " << infoPersonaje["salto"].as<int>() << "\n";
+
+    for(YAML::const_iterator itEstado = estados.begin(); itEstado != estados.end(); ++itEstado)
+    {
+        std::cout << "Encontrado estado [" << itEstado->first.as<std::string>() << "]\n";
+        for(YAML::const_iterator itAtributoEstado = itEstado->second.begin(); itAtributoEstado != itEstado->second.end(); ++itAtributoEstado)
+        {
+            if(itAtributoEstado->first.as<std::string>() == "bucle")
+            {
+                std::cout << "El bucle es de tipo [" << itAtributoEstado->second.as<std::string>() << "]\n";
+            }
+            else if(itAtributoEstado->first.as<std::string>() == "rectangulos")
+            {
+                std::cout << "Extrayendo información sobre los " << itAtributoEstado->second.size() << " rectángulos...\n";
+                for(size_t rectanguloActual = 0; rectanguloActual < itAtributoEstado->second.size(); rectanguloActual++)
+                {
+                    std::cout << "Rectángulo " << rectanguloActual << ", posición (" << itAtributoEstado->second[rectanguloActual]["posX"].as<int>() << "," << itAtributoEstado->second[rectanguloActual]["posY"].as<int>() << "), tamaño " << itAtributoEstado->second[rectanguloActual]["ancho"].as<int>() << "x" << itAtributoEstado->second[rectanguloActual]["alto"].as<int>() << ", ataque " << itAtributoEstado->second[rectanguloActual]["ataque"].as<int>() << "\n";
+                }
+            }
+            else if(itAtributoEstado->first.as<std::string>() == "frames")
+            {
+                std::cout << "Encontrados " << itAtributoEstado->second.size() << " frames: ";
+                for(size_t frameActual = 0; frameActual < itAtributoEstado->second.size(); frameActual++)
+                {
+                    std::cout << itAtributoEstado->second[frameActual] << ", ";
+                }
+            }
+            else if(itAtributoEstado->first.as<std::string>() == "sonido")
+            {
+                s
+            }
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
 
     // Los personajes y efectos son recursos que se tienen que cargar desde un principio.
     // Los recursos más sencillos (efectos de sonido, fuentes, texturas) se pueden cargar al vuelo según se
